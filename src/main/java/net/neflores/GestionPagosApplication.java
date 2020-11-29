04175c299a930f8cc251beaa513ea5079109d0ef
Nestor Flores <nestorchoele@gmail.com>
2020-11-28 23:26:51 -0300

    Correción Nombres y Referencias menores

--- src/main/java/net/neflores/GestionPagosApplication.java
+++ src/main/java/net/neflores/GestionPagosApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BpnGestionApplication  extends SpringBootServletInitializer{
+public class GestionPagosApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication.run(BpnGestionApplication.class, args);
+		SpringApplication.run(GestionPagosApplication.class, args);
 	}
 	
    // @Override
